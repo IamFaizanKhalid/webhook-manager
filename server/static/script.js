d0dd1ddc9a4751a935b6f2be02ee3c527aa64631
Faizan Khalid <IamFaizanKhalid@gmail.com>
2023-05-01 15:31:36 +0500

    Updated main view

--- server/static/script.js
+++ server/static/script.js
@@ -1,32 +1,54 @@
-$(document).ready(function() {
-    // Retrieve hooks and display them
-    $.get('http://localhost:8000/hooks', function(hooks) {
-        let hooksContainer = $('#hooks-container');
-        for (let i = 0; i < hooks.length; i++) {
-            let hook = hooks[i];
-            let card = $('<div>').addClass('col-md-4 border rounded');
-            let cardBody = $('<div>').addClass('card-body');
-            let cardTitle = $('<h5>').addClass('card-title').text(hook.id);
-            let cardText = $('<p>').addClass('card-text').html('<i class="fa fa-folder-o"></i> '+hook.command_working_directory);
-            let editButton = $('<a>').addClass('text-info mr-2').attr('href', '/edit_hook?id=' + hook.id).html('<i class="fa fa-pencil"></i>');
-            let deleteButton = $('<a>').addClass('text-danger').attr('href', '#').html('<i class="fa fa-trash"></i>');
+// Retrieve hooks and display them
+$.get('http://localhost:8000/hooks', function(hooks) {
+    let hooksContainer = $('#hooks-container');
+    for (let i = 0; i < hooks.length; i++) {
+        let hook = hooks[i];
+        let project = hook.id;
+        let directory = hook.command_working_directory;
+        let branch = hook.trigger_rule.and[1].match.value.replace('refs/heads/', '');
 
-            // Add click event to delete button
-            deleteButton.click(function(event) {
-                event.preventDefault();
-                $.ajax({
-                    url: 'http://localhost:8000/hooks/' + hook.id,
-                    type: 'DELETE',
-                    success: function() {
-                        // Remove card from display
-                        card.remove();
-                    }
-                });
+        let div = $('<div>').addClass('card col-md-4 border rounded mb-3 shadow-sm');
+        let divBody = $('<div>').addClass('card-body');
+        let divTitle = $('<h5>').addClass('card-title').text(project);
+        let folderIcon = $('<i>').addClass('fa fa-folder mr-2');
+        let branchIcon = $('<i>').addClass('fa fa-code-fork mr-2');
+        let directoryText = $('<p>').addClass('card-text').append(folderIcon).append(directory);
+        let branchText = $('<p>').addClass('card-subtitle mb-2 text-muted').append(branchIcon).append(branch);
+        let editLink = $('<a>').addClass('card-link text-info mr-2').attr('href', '/edit_hook?id=' + hook.id).html('<i class="fa fa-pencil"></i>');
+        let deleteLink = $('<a>').addClass('card-link text-danger').attr('href', '#').html('<i class="fa fa-trash"></i>');
+
+        // Add click event to delete button
+        deleteLink.click(function(event) {
+            event.preventDefault();
+            let confirmationModal = $('#confirmation-modal');
+            let confirmationButton = $('#confirmation-button');
+
+            // Set the hook id in the confirmation button data attribute
+            confirmationButton.attr('data-hook-id', hook.id);
+
+            // Show the confirmation modal
+            confirmationModal.modal('show');
+        });
+
+        // Add click event to confirmation button
+        $('#confirmation-button').click(function(event) {
+            let hookId = $(this).attr('data-hook-id');
+            $.ajax({
+                url: 'http://localhost:8000/hooks/' + hookId,
+                type: 'DELETE',
+                success: function() {
+                    // Remove div from display
+                    div.remove();
+                }
             });
 
-            cardBody.append(cardTitle, cardText, editButton, deleteButton);
-            card.append(cardBody);
-            hooksContainer.append(card);
-        }
-    });
+            // Hide the confirmation modal
+            $('#confirmation-modal').modal('hide');
+        });
+
+
+        divBody.append(divTitle, branchText, directoryText, editLink, deleteLink);
+        div.append(divBody);
+        hooksContainer.append(div);
+    }
 });
