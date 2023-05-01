5a3f9acb2ac9e0b3e6203f0378f789cb0d1522fc
Faizan Khalid <IamFaizanKhalid@gmail.com>
2023-05-01 14:33:11 +0500

    Completed Add Hook Form

--- server/static/add_hook/script.js
+++ server/static/add_hook/script.js
@@ -1,31 +1,68 @@
+$(document).ready(function () {
+    $('#hook-url').text(location.protocol + '//' + location.hostname + ":9000/hooks/my-app");
 
-$(document).ready(function() {
-    const submitButton = $('#hook-form');
+    $("#togglePassword").click(function() {
+        var password = $("#secret");
+        var icon = $(this).find("i");
+        if (password.attr("type") === "password") {
+            password.attr("type", "text");
+            icon.removeClass("fa-eye").addClass("fa-eye-slash");
+        } else {
+            password.attr("type", "password");
+            icon.removeClass("fa-eye-slash").addClass("fa-eye");
+        }
+    });
 
-    submitButton.click(function(event) {
+    $('#add-hook').click(function (event) {
         event.preventDefault();
 
         // Construct payload object
         const payload = {
             id: $('#id').val(),
-            execute_command: $('#execute-command').val(),
-            command_working_directory: $('#command-working-directory').val(),
-            response_message: $('#response-message').val(),
-            //response_headers: $('#response-headers').val(),
-            // capture_command_output: $('#capture-command-output').prop('checked'),
-            // capture_command_output_on_error: $('#capture-command-output-on-error').prop('checked'),
-            // pass_environment_to_command: $('#pass-environment-to-command').val(),
-            // pass_arguments_to_command: $('#pass-arguments-to-command').val(),
-            // pass_file_to_command: $('#pass-file-to-command').val(),
-            // json_string_parameters: $('#json-string-parameters').val(),
-            // trigger_rule: $('#trigger-rule').val(),
-            trigger_rule_mismatch_http_response_code: parseInt($('#trigger-rule-mismatch-http-response-code').val()),
-            trigger_signature_soft_failures: $('#trigger-signature-soft-failures').prop('checked'),
-            incoming_payload_content_type: $('#incoming-payload-content-type').val(),
-            success_http_response_code: parseInt($('#success-http-response-code').val()),
-            // http_methods: $('input[name="http-methods"]:checked').map(function() {
-            //     return $(this).val();
-            // }).get()
+            execute_command: $('#cmd').val(),
+            command_working_directory: $('#pwd').val(),
+            trigger_rule: {
+                and: [
+                    {
+                        match: {
+                            type: "payload-hash-sha256",
+                            secret: $('#secret').val(),
+                            parameter: {
+                                source: "header",
+                                name: "X-Hub-Signature-256"
+                            }
+                        }
+                    },
+                    {
+                        match: {
+                            type: "value",
+                            value: "refs/heads/" + $('#branch').val(),
+                            parameter: {
+                                source: "payload",
+                                name: "ref"
+                            }
+                        }
+                    }
+                ]
+            },
+            pass_arguments_to_command: [
+                {
+                    source: "payload",
+                    name: "head_commit.id"
+                },
+                {
+                    source: "payload",
+                    name: "head_commit.message"
+                },
+                {
+                    source: "payload",
+                    name: "head_commit.author.name"
+                },
+                {
+                    source: "payload",
+                    name: "head_commit.author.email"
+                }
+            ]
         };
 
         // Send POST request to API endpoint
@@ -35,12 +72,11 @@ $(document).ready(function() {
             dataType: 'json',
             contentType: 'application/json',
             data: JSON.stringify(payload),
-            success: function(data) {
-                console.log(data);
+            success: function (data) {
                 window.location.href = '/';
             },
-            error: function(error) {
-                console.error(error);
+            error: function (error) {
+                $('#error-message').text(error.responseJSON.error).css('display','inline-block');
             }
         });
     });
