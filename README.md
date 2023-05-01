974650a54f37f442ab0e59468b43f6e643c63aad
Faizan Khalid <IamFaizanKhalid@gmail.com>
2023-05-01 19:19:34 +0500

    Added README

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # webhook-manager
+
+Configure automatic deployments easily using GitHub Webhooks.
+
+<img align="right" width="120" alt="Webhook Manager" src="https://github.com/IamFaizanKhalid/webhook-manager/raw/master/server/static/logo.png">
+
+
+## Pre-requisites
+- [webhook](https://github.com/adnanh/webhook)
+
+
+## Setup
+
+- Create `.env` file like `.env.example`.
+- Set `API_KEY` variable in `.env` to use as a password.
+- Run the following command to set up this application
+    ```
+    ./scripts/setup.sh
+    ```
+
+## Usage
+
+To use _webhook-manager_ visit port `:8000` of your server, e.g. http://www.example.com:8000.
+
+To configure auto deployment for your application follow the following steps
+
+- Write a script which perform all the operations required for the deployment of your application.
+- Add a new hook in _webhook-manager_.
+- Note down your payload **URL** and **secret**.
+- Go to settings of your repository in the Webhooks section. \
+https://github.com/{user}/{repo}/settings/hooks
+- Add a new webhook and configure it.
+- All set..!
--- server/static/script.js
+++ server/static/script.js
@@ -16,15 +16,15 @@ $(document).ready(function () {
             for (let i = 0; i < hooks.length; i++) {
                 let hook = hooks[i];
                 let project = hook.id;
-                let directory = hook.command_working_directory;
+                let payloadUrl = location.protocol + '//' + location.hostname + ":9000/hooks/"+project;
                 let branch = hook.trigger_rule.and[1].match.value.replace('refs/heads/', '');
 
                 let div = $('<div>').addClass('card col-md-4 border rounded mb-3 shadow-sm');
                 let divBody = $('<div>').addClass('card-body');
                 let divTitle = $('<h5>').addClass('card-title').text(project);
-                let folderIcon = $('<i>').addClass('fa fa-folder mr-2');
+                let folderIcon = $('<i>').addClass('fa fa-globe mr-2');
                 let branchIcon = $('<i>').addClass('fa fa-code-fork mr-2');
-                let directoryText = $('<p>').addClass('card-text').append(folderIcon).append(directory);
+                let directoryText = $('<p>').addClass('card-text').append(folderIcon).append(payloadUrl);
                 let branchText = $('<p>').addClass('card-subtitle mb-2 text-muted').append(branchIcon).append(branch);
                 let editLink = $('<a>').addClass('card-link text-info mr-2').attr('href', '/edit_hook?id=' + hook.id).html('<i class="fa fa-pencil"></i>');
                 let deleteLink = $('<a>').addClass('card-link text-danger').attr('href', '#').html('<i class="fa fa-trash"></i>');
